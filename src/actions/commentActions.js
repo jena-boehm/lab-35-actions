77ea1ff4cacc3e6239a850d5839461825ffc3113
Jena Boehm <boehmjena@gmail.com>
2021-02-18 14:53:04 -0800

    create comment actions

---
+++ src/actions/commentActions.js
@@ -0,0 +1,11 @@
+export const CREATE_COMMENT = 'CREATE_COMMENT';
+export const createComment = (index, comment) => ({
+  type: CREATE_COMMENT,
+  payload: { index, comment }
+});
+
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const deleteComment = index => ({
+  type: DELETE_COMMENT,
+  payload: index
+});
